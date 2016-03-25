4593abeca01d9b3b62cffb52ad2da7c1818f9e1e
Mike Schertl <mikeschertl@googlemail.com>
2016-03-25 15:27:41 +0000

    Added contact page to nav bar

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
     def new
-        @contact = Contact.new
+        @contact = Contact.new      #Hier wird ein blankes Objekt erstellt, weches in der Variable @contact gespeichert wird. Diese Var können wir dann im View Ordner in unserem Html Doc benutzen. zb. bei dem new.html.erb !!!!!!!!!!!
     end
     
     
